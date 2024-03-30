4d6db9184d0c086b93b2ecc1f4580b814386dfe4
Sergio-1n <skuznetcov150@gmail.com>
2024-03-30 20:50:06 +0100

    Configured workspace

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 3000;
 app.use(bodyParser.json());
 
 app.listen(port, () => {
-  console.log(
-    `Server listening on ${port} and starting at http://localhost:${port}`
-  );
+    console.log(
+        `Server listening on ${port} and starting at http://localhost:${port}`
+    );
 });
